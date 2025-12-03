5889d32384ded3b7cb390192fa59570883435919
Petrichor <vikachris2007@gmail.com>
2025-12-03 22:21:34 +0300

    init

---
+++ src/main/java/ru/utmn/roadmap/RoadMapApplication.java
@@ -0,0 +1,13 @@
+package ru.utmn.roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadMapApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/utmn/roadmap/domain/entity/User.java
@@ -0,0 +1,4 @@
+package ru.utmn.roadmap.domain.entity;
+
+public class User {
+}
